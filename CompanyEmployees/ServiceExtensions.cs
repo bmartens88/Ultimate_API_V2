9df81788d54acc5d663288b9d4a803ac2d00ae57
Bas Martens <bas.martens@teamrockstars.nl>
2022-09-29 21:19:34 +0200

    Initial commit of data

---
+++ CompanyEmployees/ServiceExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Contracts;
+using LoggerService;
+
+namespace CompanyEmployees
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+            });
+
+        public static IServiceCollection ConfigureIISConfiguration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options => { });
+
+        public static IServiceCollection ConfigureLoggerService(this IServiceCollection services) =>
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+    }
+}
