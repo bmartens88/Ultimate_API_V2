a78ab66903bd3c45eb85e90a05f793de066e0b67
Bas Martens <bmartens1988@kpnmail.nl>
2022-09-30 23:08:51 +0200

    Finished chapter #4

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -8,4 +8,11 @@ public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
     public CompanyRepository(RepositoryContext context) : base(context)
     {
     }
+
+    public IEnumerable<Company> GetAllCompanies(bool trackChanges)
+    {
+        return FindAll(trackChanges)
+            .OrderBy(c => c.Name)
+            .ToList();
+    }
 }
